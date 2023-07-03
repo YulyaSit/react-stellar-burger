ed7ef114c52885fb2269a4f125ba9c99533afb58
Yuliya Sitnikova <yasilayamosh@mail.ru>
2023-07-03 14:04:21 +0300

    Шапка профиля

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -5,11 +5,11 @@ function AppHeader() {
         <header className={headerStyles.header}>
             <nav className={headerStyles.navigation}>
                 <ul className={headerStyles.list}>
-                    <li className={headerStyles.listOne}><BurgerIcon type="primary" /><p className="text text_type_main-small">Конструтор</p></li>
-                    <li className={headerStyles.listTwo}><ListIcon type="secondary" /><p className="text text_type_main-small text_color_inactive">Лента заказов</p></li>
+                    <li className={headerStyles.item1}><BurgerIcon type="primary" /><p className={`${headerStyles.item} text text_type_main-small`}>Конструтор</p></li>
+                    <li className={headerStyles.item2}><ListIcon type="secondary" /><p className={`${headerStyles.item} text text_type_main-small text_color_inactive`}>Лента заказов</p></li>
                 </ul>
                 <Logo />
-                <div className={headerStyles.profile}><ProfileIcon type="secondary" /><p className="text text_type_main-small text_color_inactive">Личный кабинет</p></div>
+                <div className={headerStyles.profile}><ProfileIcon type="secondary" /><p className={`${headerStyles.item} text text_type_main-small text_color_inactive`}>Личный кабинет</p></div>
             </nav>
         </header>
     )
